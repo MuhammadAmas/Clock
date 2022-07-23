07e9403b2f9b23e79f60b8288c7f167c2950fb61
Muhammad Amas Waseem <84465512+AmasWaseem@users.noreply.github.com>
2022-07-23 14:01:26 +0500

    Updated

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
+// Analog Clock
+function analogClockTime(){
+    let dateTime = new Date();
+    let hours = dateTime.getHours() * 30;
+    let minutes = dateTime.getMinutes()*6;
+    let seconds =  dateTime.getSeconds()*6;
 
+    const hr = document.querySelector('.hr');
+    const min = document.querySelector('.min');
+    const sec = document.querySelector('.sec');
+
+    hr.style.transform = `rotateZ(${(hours)+ (minutes/12)}deg)`;
+    min.style.transform = `rotateZ(${(minutes)}deg)`;
+    sec.style.transform = `rotateZ(${(seconds)}deg)`;
+}
+setInterval(analogClockTime, 1000);
+
+
+
+
+
+//Digital Clock
 function time() {
     let dateTime = new Date();
     let hours = dateTime.getHours();
@@ -6,6 +27,12 @@ function time() {
     let seconds =  dateTime.getSeconds();
     let meridian = 'AM';
     let day = dateTime.getDay();
+    let date = dateTime.getDate();
+    let month = dateTime.getMonth();
+    let year = dateTime.getFullYear();
+
+    console.log(month)
+    let monthArr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
     let dayArr = ['Sunday','Monday','Tueday','Wednesday','Thursday','Friday','Saturday']
     if(hours >= 12){
         hours = hours-12;
@@ -17,13 +44,16 @@ function time() {
     seconds = (seconds <10)?"0" + seconds:seconds;
 
     let presentTime = `${hours}: ${minutes}: ${seconds} ${(meridian)}`;
-    let presentDay = `${dayArr[day]}`
+    let presentDay = `${dayArr[day]}`;
+    let presentDate = `${date} ${monthArr[month]} ${year}`
+    console.log(presentDate);
 
     document.getElementById('display-clock').innerText = presentTime;
     document.getElementById('present-day').innerText = presentDay;
+    document.getElementById('date').innerText = presentDate;
 }
 setInterval(time,1000);
 
-// time();
+
 
 
